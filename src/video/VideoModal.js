b4554337b01c9a5df35cd1e49cf349302941f1ac
Steven Martin <stevenisekimartin@gmail.com>
2017-02-15 22:31:01 +1100

    Fix insert data block import

--- src/video/VideoModal.js
+++ src/video/VideoModal.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components'
-import insertDataBlock from '../../utils/insertDataBlock'
+import insertDataBlock from './insertDataBlock'
 import getVideoUrl from './getVideoUrl'
 
 export default class extends Component {
